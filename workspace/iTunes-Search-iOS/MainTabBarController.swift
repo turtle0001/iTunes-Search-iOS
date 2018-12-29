bd2bf2ebabdefeb31348124c0f6c3f7feaa9c4ee
Joash Tubaga <joashtubaga@gmail.com>
2018-12-29 13:34:22 +0800

    [✨] Added MainController
    * Modified the delegate file to handle main controller file screen programmatically.
    * Added the Main Tab Bar Controller
    * Added Tab Bar icons

--- workspace/iTunes-Search-iOS/MainTabBarController.swift
+++ workspace/iTunes-Search-iOS/MainTabBarController.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2018 joashtubaga. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .green
+    
+    UINavigationBar.appearance().prefersLargeTitles = true
+    
+    tabBar.tintColor = .purple
+    
+    setupViewControllers()
+  }
+  
+  //MARK:- Setup Functions
+  
+  func setupViewControllers() {
+    viewControllers = [
+      generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+      generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+      generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+    ]
+  }
+  
+  //MARK:- Helper Functions
+  
+  fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+    let navController = UINavigationController(rootViewController: rootViewController)
+    //        navController.navigationBar.prefersLargeTitles = true
+    rootViewController.navigationItem.title = title
+    navController.tabBarItem.title = title
+    navController.tabBarItem.image = image
+    return navController
+  }
+  
+}
